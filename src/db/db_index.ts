14b590eb6073c82e5971c3c2cb13284c8f165208
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-05-21 11:52:48 +0600

    successfully connet and users table create

--- src/db/db_index.ts
+++ src/db/db_index.ts
@@ -3,19 +3,28 @@ import config from '../config/index_config'
 
 
 export const pool = new Pool({
-    connectionString: config.databaseUrl
+    connectionString: config.databaseUrl,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 export const initDB = async () => {
 
-    pool.query(`
+    try {
+        await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(30) NOT NULL,
-        email VARCHAR(40) UNIQUE NOT NULL
+        email VARCHAR(40) UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL,
+        role VARCHAR(15) DEFAULT 'contributor' NOT NULL,
+        created_at TIMESTAmP NOT NULL,
+        updated_at TIMESTAMP NOT NULL
         )`);
-
-
-    console.log("Database connected...");
+        console.log("Database connected...");
+    } catch (error) {
+        console.log("Database connection error:", error);
+    }
 
 }
